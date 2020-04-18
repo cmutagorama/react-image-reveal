fea7134a7fba13dfcea7d2df16570efbb11d3098
cmutagorama <cmutagorama@gmail.com>
2020-04-18 15:31:56 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect } from "react";
+import "./App.scss";
+import people from "./img/people.webp";
+import CSSRulePlugin from "gsap/CSSRulePlugin";
+import { gsap, TimelineLite, Power2 } from "gsap";
 
-function App() {
+gsap.registerPlugin(CSSRulePlugin);
+
+const App = () => {
+  let container = useRef(null);
+  let image = useRef(null);
+  let imageReveal = CSSRulePlugin.getRule(".img-container:after");
+
+  const tl = new TimelineLite();
+
+  useEffect(() => {
+    tl.to(container, 0, { visibility: "visible" })
+      .to(imageReveal, 1.4, {
+        width: "0%",
+        ease: Power2.easeInOut,
+      })
+      .to(image, 1.4, { scale: 1.6, ease: Power2.easeInOut, delay: -1.6 });
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="main">
+      <div className="container" ref={(el) => (container = el)}>
+        <>
+          <div className="img-container">
+            <img src={people} alt="People" ref={(el) => (image = el)} />
+          </div>
+        </>
+      </div>
+    </section>
   );
-}
+};
 
 export default App;
